3b8e6e9b86d2a162e321f4f11a07a90fc72b68e6
chriteli1 <73424544+chriteli1@users.noreply.github.com>
2022-05-16 14:40:32 +0300

    Add files via upload
    Changed upload type (from saved file to url).

--- back-end.py
+++ back-end.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
 import os, os.path
+import json
+from urllib.request import urlopen
 
 
-UPLOAD_FOLDER = 'C:/Users/Chris Telioglanidis/Desktop/Back-end/uploads' #Path to save the uploaded file
+#UPLOAD_FOLDER = 'C:/Users/Chris Telioglanidis/Desktop/Back-end/uploads' #Path to save the json file
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
@@ -17,17 +18,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def hello():
     return "Hello! Go to /upload."
 
-@app.route('/upload') #File select screen
+@app.route('/upload') #Url input screen
 def upload_file():
     return render_template('back-end.html')
 
 
-@app.route('/uploader', methods=['GET', 'POST']) #File uploaded screen
+@app.route('/uploader', methods=['POST', 'GET']) #Url sent screen
 def uploader():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-        return 'file uploaded successfully'
+        f = request.form['url']
+        with urlopen(f) as response:
+            source = response.read()
+        #f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        data = json.loads(source)
+    return 'File uploaded successfully.'
 
 
 if __name__ == '__main__':
