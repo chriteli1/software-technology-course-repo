61676fb6878f50105650c241963083334d3fa6d8
charverr <105058897+charverr@users.noreply.github.com>
2022-06-20 19:53:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,49 +2,65 @@ from urllib.request import urlopen
 from flask import Flask, request, render_template, Blueprint, jsonify
 import mysql.connector
 import json
+from flask_cors import CORS
+
+
+
+# https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php#earthquakes
 
 app = Flask(__name__)
+CORS(app)
 
+# mydb = mysql.connector.connect(host="dalab.ee.duth.gr", user="s57330", password="114660", database="s57330")
+# mycursor = mydb.cursor()
 
-with urlopen("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson") as response:
-    source = response.read()
-
-data = json.loads(source)
-object=json.dumps(data, indent=4)
-
-
-mydb = mysql.connector.connect(host="dalab.ee.duth.gr", user="s57330", password="114660", database="s57330")
+mydb = mysql.connector.connect(host="dalab.ee.duth.gr", user="s57341", password="114682", database="s57341")
 mycursor = mydb.cursor()
 
-# for x in data['features']:
-#     magnitude = x['properties']['mag']
-#     place = x['properties']['place']
-#     time = x['properties']['time']
-#     #print(magnitude, place, time)
-#     sql = "insert into front_end(mag,place,time) values(%s,%s,%s)"
-#     val = [magnitude, place, time]
-#     mycursor.execute(sql, val)
-#     mydb.commit()
 
+#BackEnd connection
+@app.route('/api/import/', methods=['POST'])
+def dataReceived():
+    magnitude = float(request.args['mag'])
+    place = request.args['place']
+    time = request.args['time']
+    flag = int(request.args['flag']) #Is equal to 0 if db is not empty and 1 if it is
+    eq_id = "123t"
+    #=====Empty table if new json is uploaded=====#
+    if flag == 0:
+        sql0 = "truncate table earthquakes"
+        mycursor.execute(sql0)
+        mydb.commit()
+    # ============================================#
 
-@app.route('/significant_magnitude/<float:min> & <float:max>')
+    #=====Send new data=====#
+    sql = "insert into earthquakes (mag,place,time,eq_id) values(%s,%s,%s,%s)"
+    val = (magnitude, place, time, eq_id)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    # ======================#
+
+    return "Success!"
+
+@app.route('/significant_magnitude/<float:min>&<float:max>', methods=['GET'])
 def significant_magnitude(min, max):
-    min = float(min)
-    max = float(max)
-    if min < max:
-        a = "SELECT mag, place, time FROM front_end WHERE mag >= %s AND mag <= %s "
-        b = (min, max)
-        mycursor.execute(a, b)
-        myresult = mycursor.fetchall()
-        earthquakes = []
-        content = {}
-        for rv in myresult:
-            content = {'mag': rv[0], 'place': rv[1], 'date': rv[2]}
-            earthquakes.append(content)
-            #content = {}
-        return jsonify(earthquakes)
-    else:
-        print("min prepei na exei mikrotero value apo to max")
+    if request.method == 'GET':
+        min = float(min)
+        max = float(max)
+        if min < max:
+            a = "SELECT mag, place, time FROM earthquakes WHERE mag >= %s AND mag <= %s "
+            b = (min, max)
+            mycursor.execute(a, b)
+            myresult = mycursor.fetchall()
+            earthquakes = []
+            content = {}
+            for rv in myresult:
+                content = {'mag': rv[0], 'place': rv[1], 'date': rv[2]}
+                earthquakes.append(content)
+                #content = {}
+            return jsonify(earthquakes)
+        else:
+            print("min prepei na exei mikrotero value apo to max")
     return
 
 
@@ -53,7 +69,7 @@ def significant_magnitude(min, max):
 
 @app.route('/location/<string:loc>')
 def location(loc):
-    c = "SELECT mag, place, time FROM front_end WHERE place = %s"
+    c = "SELECT mag, place, time FROM earthquakes WHERE place = %s"
     d = [loc]
     mycursor.execute(c, d)
     myresult = mycursor.fetchall()
@@ -68,10 +84,10 @@ def location(loc):
 #location('55 km WSW of Masachapa, Nicaragua')
 
 
-@app.route('/date/<string:dat>')
-def date(dat=''):
-    e = "SELECT mag, place, time FROM front_end WHERE time = %s"
-    f = [dat]
+@app.route('/date/<string:min>&<string:max>')
+def date(min, max):
+    e = "SELECT mag, place, time FROM earthquakes WHERE time >= %s AND time <= %s"
+    f = (min, max)
     mycursor.execute(e, f)
     myresult = mycursor.fetchall()
     earthquakes = []
